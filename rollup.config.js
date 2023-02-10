0871c3df85ca0629d60348283e4dc4a360e2272c
afe1 <2279948211@qq.com>
2023-02-10 17:16:17 +0800

    多产物配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,16 @@
 
 const buildOptions = {
   input: ["src/index.js"],
-  output: {
-    dir: "dist/es",
-    format: "esm",
-  },
+  output: [
+    {
+      dir: "dist/es",
+      format: "esm",
+    },
+    {
+      dir: "dist/es",
+      format: "cjs",
+    },
+  ],
 };
 
 export default buildOptions;
